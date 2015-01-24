5c4cced86835e55bd1e9996426fed4bb7f3da57c
Blake Hagen <blakehagen@gmail.com>
2015-01-24 10:44:04 -0700

    sat 1045am

--- arrayReview.js
+++ arrayReview.js
@@ -10,19 +10,23 @@ var last = function() {
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
 
+var removeOdds = function(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 !== 0) {
+			arr.splice(i, 1);
+			i--;
+		}
+	}
+};
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
-
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full of numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
@@ -30,10 +34,17 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
 
+var random = function(arr) {
+	var randomNum = getRandomArbitrary();
+	console.log(randomNum);
+	if(arr.indexOf(randomNum) !== -1) {
+			alert(true);
+	} else {
+		alert(false);
+	}
+};
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
 
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
